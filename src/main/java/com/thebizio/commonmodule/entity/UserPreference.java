11a3a26be36c4b9e76f93ad265749f150b5ecac1
Dheeraj <dheerajjaynath.vishwakarma@digitalairtech.com>
2023-04-27 12:38:05 +0530

    user-preference entity added

---
+++ src/main/java/com/thebizio/commonmodule/entity/UserPreference.java
@@ -0,0 +1,30 @@
+package com.thebizio.commonmodule.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "userpreferences")
+@Getter
+@Setter
+public class UserPreference extends LastUpdateDetail{
+
+    @Id
+    @GeneratedValue(generator = "uuid4")
+    @Column(columnDefinition = "uuid")
+    private UUID id;
+
+    private String timeZone;
+    private String language;
+    private String countryLocale;
+    private String dateFormat;
+    private String timeFormat;
+    private String numberFormat;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
