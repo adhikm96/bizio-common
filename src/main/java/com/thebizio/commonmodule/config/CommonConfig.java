84ea143cf6b52050a0b2234d7f098232707633b3
Amit <amit.k@digitalairtech.com>
2023-07-05 17:23:46 +0530

    string deserializer added

--- src/main/java/com/thebizio/commonmodule/config/CommonConfig.java
+++ src/main/java/com/thebizio/commonmodule/config/CommonConfig.java
@@ -3,9 +3,7 @@ package com.thebizio.commonmodule.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import net.avalara.avatax.rest.client.AvaTaxClient;
-import net.avalara.avatax.rest.client.enums.AvaTaxEnvironment;
-import org.springframework.beans.factory.annotation.Value;
+import com.thebizio.commonmodule.deserializer.TrimmedStringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -16,11 +14,17 @@ import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class CommonConfig {
+
     @Bean
-    ObjectMapper objectMapper() {
-        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder();
+    }
+
+    @Bean
+    ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
         builder.serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
         builder.serializerByType(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE));
+        builder.deserializerByType(String.class, new TrimmedStringDeserializer());
         return builder.build();
     }
 }
---
+++ src/main/java/com/thebizio/commonmodule/deserializer/TrimmedStringDeserializer.java
@@ -0,0 +1,21 @@
+package com.thebizio.commonmodule.deserializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
+
+import java.io.IOException;
+
+public class TrimmedStringDeserializer extends StringDeserializer {
+
+    @Override
+    public String deserialize(JsonParser parser, DeserializationContext context) throws IOException {
+        String value = super.deserialize(parser, context);
+        if (value != null) {
+            value = value.trim();
+        }
+
+        System.out.println("I am called");
+        return value;
+    }
+}
