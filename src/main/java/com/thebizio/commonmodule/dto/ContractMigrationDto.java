eca7cf0269411088036ff53c341974ff0bbf1412
nick9822 <nick9822@gmail.com>
2023-12-13 02:16:17 +0530

    Added ContractMigrationDto

---
+++ src/main/java/com/thebizio/commonmodule/dto/ContractMigrationDto.java
@@ -0,0 +1,12 @@
+package com.thebizio.commonmodule.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContractMigration {
+    private UUID id;
+}
