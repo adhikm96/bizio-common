02a64113c4f477e2628f9fe4b859839360e2408b
Amit <amit.k@digitalairtech.com>
2024-01-17 19:23:22 +0530

    fix

--- src/main/resources/db/migration/V7__Create_overdue_suspended_sub_table.sql
+++ src/main/resources/db/migration/V7__Create_overdue_suspended_sub_table.sql
@@ -9,5 +9,5 @@ create materialized view if not exists ots_subscriptions as (
     join organizations org on sub.org_id = org.id
     join subscription_users su on su.subscription_id = sub.id
     join users u on u.id = su.user_id
-    where sub.subscription_status in (1,2,3) and u.status =
+    where sub.subscription_status in (1,2,3)
 );
