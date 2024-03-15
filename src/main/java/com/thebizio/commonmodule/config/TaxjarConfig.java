78eefc81f32e9363a10e5fc00b60a71c753553e8
Amit <amit.k@digitalairtech.com>
2024-03-15 15:11:54 +0530

    address api call on non-dev env

--- src/main/java/com/thebizio/commonmodule/config/TaxjarConfig.java
+++ src/main/java/com/thebizio/commonmodule/config/TaxjarConfig.java
@@ -1,6 +1,7 @@
 package com.thebizio.commonmodule.config;
 
 import com.taxjar.Taxjar;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +9,10 @@ import org.springframework.context.annotation.Configuration;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.thebizio.commonmodule.service.tax.TaxJarService.DEVELOPMENT_ENV;
+
 @Configuration
+@Slf4j
 public class TaxjarConfig {
     private final String taxJarApiToken;
 
@@ -23,6 +27,11 @@ public class TaxjarConfig {
     public Taxjar taxjar() {
         Map<String, Object> params = new HashMap<>();
         params.put("x-api-version", taxJarApiVersion);
+
+        if(System.getenv("BIZIO_ENV") != null && System.getenv("BIZIO_ENV").equals(DEVELOPMENT_ENV)) {
+            params.put("apiUrl", Taxjar.SANDBOX_API_URL);
+            log.info("using sandbox for taxjar api");
+        }
         return new Taxjar(taxJarApiToken, params);
     }
 }
