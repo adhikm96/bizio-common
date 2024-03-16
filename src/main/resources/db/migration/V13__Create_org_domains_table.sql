03225fabc35743c0c2ae1769549f98cbf304ebd1
Amit <amit.k@digitalairtech.com>
2024-03-16 15:23:13 +0530

    migration updates

--- src/main/resources/db/migration/V13__Create_org_domains_table.sql
+++ src/main/resources/db/migration/V13__Create_org_domains_table.sql
@@ -1,1 +1,2 @@
 create table org_domains (id uuid not null, created_at timestamp default CURRENT_TIMESTAMP not null, created_by varchar(255), modified_at timestamp, modified_by varchar(255), domain varchar(255), organization_id uuid, primary key (id));
+alter table org_domains add constraint FKh1xjn84phjm3iqp1pxcg557ob foreign key (organization_id) references organizations;
