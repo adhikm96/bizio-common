c84aaffb3f6382640662d6280f8620abc5a28d7c
adhik.mehta <adhik.mehta@digitalairtech.com>
2024-08-09 16:52:40 +0530

    migration file change

--- src/main/resources/db/migration/V25__Add_sub__name_in_org_contract_table.sql
+++ src/main/resources/db/migration/V25__Add_sub__name_in_org_contract_table.sql
@@ -1,4 +1,4 @@
 ALTER TABLE org_contracts
-ADD COLUMN IF NOT EXISTS sub_name character varying(255) pg_catalog."default" NOT NULL,
+ADD COLUMN IF NOT EXISTS sub_name character varying(255) NOT NULL,
 DROP CONSTRAINT IF EXISTS org_contracts_pkey,
 ADD CONSTRAINT org_contracts_pkey PRIMARY KEY (org_code, product_code, sub_name);
